62b8e9120328def16872b96d24959ab47a3c7efe
Michael Montes <mich.montes@gmail.com>
2021-05-14 11:34:27 -0600

    inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. jj hhh
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, hola plasticola
         </a>
       </header>
     </div>
